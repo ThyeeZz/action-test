2a68ad930eb4041c5dedea760ee229f70d931ba2
ThyeeZz <jialian.ji@zilliz.com>
2025-04-14 14:10:17 +0800

    test 10

---
+++ scripts/create-pr.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+
+
+const content = fs.readFileSync(join(process.cwd(), 'README.md'), 'utf8');
+
+const newContent = content + '\n this is test 10'
+
+fs.writeFileSync(join(process.cwd(), 'README.md'), newContent);
+
+console.log('Done');
