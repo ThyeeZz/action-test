c996ce036c3481b5c10bb7f94594470f1fe3be9f
ThyeeZz <jialian.ji@zilliz.com>
2025-04-14 16:29:30 +0800

    test 25

---
+++ scripts/make-change.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const {join} = require('path');
+
+
+
+const content = fs.readFileSync(join(process.cwd(), 'README.md'), 'utf8');
+
+const newContent = content + '\n this is test 25'
+
+fs.writeFileSync(join(process.cwd(), 'README.md'), newContent);
+
+console.log('Done');
