8b707764f7acadbc14ee37b1db68f63f0fb85407
ThyeeZz <jialian.ji@zilliz.com>
2025-04-14 14:15:16 +0800

    test 12

--- scripts/create-pr.js
+++ scripts/create-pr.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const content = fs.readFileSync(join(process.cwd(), 'README.md'), 'utf8');
 
-const newContent = content + '\n this is test 11'
+const newContent = content + '\n this is test 12'
 
 fs.writeFileSync(join(process.cwd(), 'README.md'), newContent);
 
